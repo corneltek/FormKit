848d97b5497d68afa306d5cbcc812b6f1a51abad
c9s <cornelius.howl@gmail.com>
2012-03-27 16:51:46 +0800

    FormKit files

---
+++ src/FormKit/FormKit.php
@@ -0,0 +1,18 @@
+<?php
+namespace FormKit;
+
+/**
+ * Form Widget Factory class
+ */
+class FormKit
+{
+
+    static function input( $name, $attributes = array() )
+    {
+    }
+
+    static function select($name, $attributes = array() )
+    {
+    }
+
+}
