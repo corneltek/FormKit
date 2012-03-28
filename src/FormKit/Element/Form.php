7aa3dbe3863c9197291b20bba4ccc45b5e08e4a4
c9s <cornelius.howl@gmail.com>
2012-03-29 00:34:19 +0800

    Add Form Element

---
+++ src/FormKit/Element/Form.php
@@ -0,0 +1,23 @@
+<?php
+namespace FormKit\Element;
+use FormKit\Element;
+
+class Form extends Element
+{
+	/**
+	 * application/x-www-form-urlencoded
+     * multipart/form-data
+     * text/plain
+	 */
+	public $class = array('formkit-form');
+	public $enctype = 'multipart/form-data';
+
+    public function render()
+    {
+        return '<form' . $this->_renderAttributes(
+                    array('id','class','action','name','method','enctype','target')) . '>'
+            . $this->_renderChildren()
+            . '</form>';
+    }
+}
+
