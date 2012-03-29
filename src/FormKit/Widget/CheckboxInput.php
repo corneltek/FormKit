6f117ba4f3ce1b33ede4378b1124b54b7f1eca2b
c9s <cornelius.howl@gmail.com>
2012-03-29 12:46:19 +0800

    reindent code.

--- src/FormKit/Widget/CheckboxInput.php
+++ src/FormKit/Widget/CheckboxInput.php
@@ -12,40 +12,48 @@ class CheckboxInput extends BaseWidget
     public $class = array('formkit-widget','formkit-checkbox');
     public $type = 'checkbox';
 
-	public function init() 
-	{ 
-		$this->type = 'checkbox';
-		$this->value = 1;
-   	}
+    public function init() 
+    { 
+        $this->value = 1;
+    }
 
-	public function check( $value = null ) 
-	{
-        if( $value !== null )
+    public function check( $value = null ) 
+    {
+        if( $value !== null ) {
             $this->checked = $value;
-        else
+        } else {
             $this->checked = true;
-	}
+        }
+        return $this;
+    }
 
-	public function uncheck()
-	{
-		$this->uncheck = false;
-	}
+    public function uncheck()
+    {
+        $this->checked = false;
+        return $this;
+    }
+
+    public function toggleCheck()
+    {
+        $this->checked = ! $this->checked;
+        return $this;
+    }
 
     public function render()
     {
-		ob_start();
-		$fieldId = $this->name . '-' . time();
-		?><input id="<?= $fieldId ?>" type="hidden" 
-			name="<?=$this->name?>" 
-			value="<?= ($this->value) ? $this->value : ''; ?>"/>
-		<input type="<?= $this->type ?>" <?=$this->_renderBasicAttributes(); ?> 
-			<?php if( $this->checked ): ?>checked<?php endif ?> onclick=" 
-				var el = document.getElementById('<?= $fieldId ?>');
-					el.value = (el.value == '<?= $this->value ?>') ? '' : '<?= $this->value ?>';
-			"/>
-		<?php
-		$html = ob_get_contents();
-		ob_end_clean();
+        ob_start();
+        $fieldId = $this->name . '-' . time();
+        ?><input id="<?= $fieldId ?>" type="hidden" 
+            name="<?=$this->name?>" 
+            value="<?= ($this->value) ? $this->value : ''; ?>"/>
+        <input <?=$this->_renderAttributes(array('type','class','id')); ?> 
+            <?php if( $this->checked ): ?>checked<?php endif ?> onclick=" 
+                var el = document.getElementById('<?= $fieldId ?>');
+                    el.value = (el.value == '<?= $this->value ?>') ? '' : '<?= $this->value ?>';
+            "/>
+        <?php
+        $html = ob_get_contents();
+        ob_end_clean();
         return $html;
     }
 }
