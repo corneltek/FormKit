adffbafd0bdce3890dfdce974bb34bc7e1a1713f
c9s <cornelius.howl@gmail.com>
2012-03-30 11:49:15 +0800

    reindent

--- src/FormKit/Widget/Submit.php
+++ src/FormKit/Widget/Submit.php
@@ -5,13 +5,16 @@ use FormKit\Widget\TextInput;
 class Submit extends TextInput
 {
 
-	public $class = array('formkit-widget','formkit-submit');
-	public $type = 'submit';
+    public $class = array('formkit-widget','formkit-submit');
+    public $type = 'submit';
 
-	public function __construct( $name = null , $attributes = array() )
-	{
-		parent::__construct($name, $attributes );
-	}
+    /**
+     * a submit button usually does not have a name
+     */
+    public function __construct( $name = null , $attributes = array() )
+    {
+        parent::__construct($name, $attributes );
+    }
 
 }
 
