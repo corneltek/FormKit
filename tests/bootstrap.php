8263827670bf8f8b266961b2202da2252c5e1007
c9s <cornelius.howl@gmail.com>
2012-06-01 15:39:41 +0800

    use PHPUnit/TestMore.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 define('LIBROOT', dirname(__DIR__) );
-require LIBROOT . '/tests/helpers.php';
+require 'PHPUnit/TestMore.php';
 require LIBROOT . '/vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
 $loader = new \Universal\ClassLoader\BasePathClassLoader( array(
     LIBROOT . '/src',
