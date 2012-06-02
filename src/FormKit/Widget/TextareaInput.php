b048db8e724e5a14e564963b30eb54612c480de5
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2012-06-02 14:48:41 +0800

    TextareaInput & corresponding demo

---
+++ src/FormKit/Widget/TextareaInput.php
@@ -0,0 +1,56 @@
+<?php
+namespace FormKit\Widget;
+
+class TextareaInput extends BaseWidget
+{
+    public $tag;
+    public $class = array('formkit-widget','formkit-textarea');
+    public $type = 'textarea';
+    public $value;
+    public $size;
+    public $alt;
+    public $readonly;
+    public $style;
+
+
+    /**
+     * Render Widget with attributes
+     *
+     * @param array $attributes
+     * @param string HTML string
+     */
+    public function render( $attributes = array() )
+    {
+        $this->setAttributes( $attributes );
+        return '<textarea' 
+            . $this->_renderAttributes(array(
+                'class','id','type',
+                'name','cols','rows',
+                'readonly','style',
+                'disabled',
+
+                /* Event Attributes */
+                'onblur',
+                'onchange',
+                'onclick',
+                'ondblclick',
+                'onfocus',
+                'onmousedown',
+                'onmousemove',
+                'onmouseout',
+                'onmouseover',
+                'onmouseup',
+                'onkeydown',
+                'onkeypress',
+                'onkeyup',
+                'onselect',
+            ))
+            . '>'
+            . htmlspecialchars($this->value, ENT_NOQUOTES, 'UTF-8')
+            . '</textarea>' 
+            . $this->renderHint();
+    }
+
+}
+
+
