f43fdef8f6a893a5f3f1ecf2508bcf115499449c
c9s <cornelius.howl@gmail.com>
2012-07-31 23:36:38 +0800

    Add ImageFileInput

---
+++ src/FormKit/Widget/ImageFileInput.php
@@ -0,0 +1,10 @@
+<?php
+namespace FormKit\Widget;
+
+class ImageFileInput extends FileInput
+{
+    public $type = 'file';
+    public $class = array('formkit-widget','formkit-widget-imagefile');
+}
+
+
