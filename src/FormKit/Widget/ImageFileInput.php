4e19e9a04f7a2e83b8992297019aba510f1637b1
c9s <cornelius.howl@gmail.com>
2012-08-02 10:40:09 +0800

    Document ImageFileInput

--- src/FormKit/Widget/ImageFileInput.php
+++ src/FormKit/Widget/ImageFileInput.php
@@ -1,18 +1,40 @@
 <?php
 namespace FormKit\Widget;
+use FormKit\Element;
+
+
+/**
+ *
+ * $input = new ImageFileInput('image');
+ * $input->image->align = 'right';
+ * $input->image->src = '.....';
+ * $input->imageWrapper->setAttributeValues(....);
+ * $input->render();
+ *
+ */
 
 class ImageFileInput extends FileInput
 {
     public $type = 'file';
     public $class = array('formkit-widget','formkit-widget-imagefile');
+    public $image;
+    public $imageWrapper;
+
+    function init() {
+        parent::init();
+        if( $this->value ) {
+            $this->imageWrapper = new Element('div',array('class' => 'formkit-image-cover'));
+            $this->image = new Element('img',array(
+                'src' => $this->value,
+            ));
+            $this->imageWrapper->append($this->image);
+        }
+    }
 
     function render() 
     {
-        if( $this->value ) {
-            $img = new \FormKit\Element('img',array( 
-                'src' => 'new-google-chrome-logo.jpg'
-            ));
-            // var_dump( $img->render() );
+        if( $this->image ) {
+            return $this->imageWrapper->render() . parent::render();
         }
         return parent::render();
     }
