3fe84bba9c22b5d7f6f0338a88806b38025bebbf
c9s <cornelius.howl@gmail.com>
2012-08-21 15:07:42 +0800

    jsimagefile: use FormKit.register API to hook js widget handlers

--- assets/formkit/jsimagefile/jsimagefile.js
+++ assets/formkit/jsimagefile/jsimagefile.js
@@ -1,30 +1,28 @@
-jQuery(function($){
-    function poll(){
-        $('.formkit-widget-imagefile').not('.done').each(function(){
-            var $this = $(this);
-            $this.addClass('done');
-            if( $this.data('url') ){
-                var src = $this.data('url'),
-                    image = new Image;
-                image.onload = function(){
-                    var width = parseInt($this.data('max-width'), 10),
-                        height = parseInt($this.data('max-height'), 10);
-                    if( width && image.width > width )
-                        image.height *= width / image.width;
-                    if( height && image.height > height )
-                        image.width *= height / image.height;
-                    image.border = 0;
-                    $this.after($('<a>')
-                        .attr('href', src)
-                        .attr('target', '_blank')
-                        .append($(image))
-                        .insertAfter($this))
-                    .after('<br/>');
-                };
-                image.src = src;
-            }
-        });
-        setTimeout(poll, 300);
-    }
-    poll();
-})
+FormKit.register(function(e,scopeEl) {
+    $(scopeEl).find('.formkit-widget-imagefile').each(function(){
+        var $this = $(this);
+        if( $this.hasClass('done') )
+            return;
+        $this.addClass('done');
+        if( $this.data('url') ){
+            var src = $this.data('url'),
+                image = new Image;
+            image.onload = function(){
+                var width = parseInt($this.data('max-width'), 10),
+                    height = parseInt($this.data('max-height'), 10);
+                if( width && image.width > width )
+                    image.height *= width / image.width;
+                if( height && image.height > height )
+                    image.width *= height / image.height;
+                image.border = 0;
+                $this.after($('<a>')
+                    .attr('href', src)
+                    .attr('target', '_blank')
+                    .append($(image))
+                    .insertAfter($this))
+                .after('<br/>');
+            };
+            image.src = src;
+        }
+    });
+});
