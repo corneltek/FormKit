20d20e66abef3c144c609e1e7aecf0377d0e404f
c9s <cornelius.howl@gmail.com>
2012-09-02 16:43:32 +0800

    Add ThumbImageFileInput

---
+++ src/FormKit/Widget/ThumbImageFileInput.php
@@ -0,0 +1,42 @@
+<?php
+namespace FormKit\Widget;
+use FormKit\Element;
+
+/**
+ *
+ * $input = new ImageFileInput('image');
+ * $input->image->align = 'right';
+ * $input->image->src = '.....';
+ * $input->imageWrapper->setAttributeValues(....);
+ * $input->render();
+ *
+ */
+class ThumbImageFileInput extends TextInput
+{
+    public $type = 'file';
+    public $class = array('formkit-widget','formkit-widget-thumbimagefile');
+
+    public $image;
+    public $imageWrapper;
+
+    function init() {
+        parent::init();
+        if( $this->value ) {
+            $this->imageWrapper = new Element('div',array('class' => 'formkit-image-cover'));
+            $this->image = new Element('img',array(
+                'src' => $this->value,
+            ));
+            $cutDiv = new Element('div',array('class' => 'cut') );
+            $cutDiv->append($this->image);
+            $this->imageWrapper->append($cutDiv);
+        }
+    }
+
+    function render($attributes = array() ) 
+    {
+        if( $this->image ) {
+            return $this->imageWrapper->render() . parent::render($attributes);
+        }
+        return parent::render($attributes);
+    }
+}
