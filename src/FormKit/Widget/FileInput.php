cc8dd9dba4142360135d150263def7a7dd95f8dc
c9s <cornelius.howl@gmail.com>
2013-02-14 22:55:33 +0800

    define ignored attribute names in FileInput

--- src/FormKit/Widget/FileInput.php
+++ src/FormKit/Widget/FileInput.php
@@ -5,5 +5,9 @@ class FileInput extends TextInput
 {
     public $type = 'file';
     public $class = array('formkit-widget','formkit-widget-file');
+
+    // we need to ignore it, because browsers might change the value to 
+    // something like 'C:\fakepath'
+    public $_ignoredAttributes = array('value');
 }
 
