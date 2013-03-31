c7a9105f9df4a615b360e87d612d082e49b4bad0
c9s <cornelius.howl@gmail.com>
2013-04-01 00:37:00 +0800

    Add GridLayoutTest

---
+++ tests/FormKit/Layout/GridLayoutTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class GridLayoutTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $fooText = new FormKit\Widget\TextInput('foo[]');
+        $barText = new FormKit\Widget\TextInput('bar[]');
+
+        $gridLayout = new FormKit\Layout\GridLayout();
+        $gridLayout->setHeaderLabels(array( $fooText ));
+
+        $gridLayout->insertWidgetsByRow( array($fooText, $barText) );
+
+        $html = $gridLayout->render();
+        $dom = new DOMDocument;
+        $dom->loadHTML($html);
+        ok($dom);
+    }
+}
+
